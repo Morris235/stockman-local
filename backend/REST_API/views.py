4c7c87f748e809514aaf531e655712b7ca8664db
morris <dltkdah777@gmail.com>
2021-07-02 14:25:21 +0900

    REST : 세부 쿼리스트링 검색 적용

--- backend/REST_API/views.py
+++ backend/REST_API/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, status
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter
 from .models import CompanyInfo, DailyPrice, CompanyState
 from .serializers import CompSerializer, DailyPriceSerializer, CompanyStateSerializer
-
+import logging
 
 # rest_framework에 있는 viewset에서 modelViewSet을 가져와 연동시킨다.
 # seriallizer틀을 먼저 작성해야함
@@ -50,13 +52,14 @@ class CompViewSet(viewsets.ModelViewSet):
         code_request = self.request.query_params.get("code", None)
         com_name = self.request.query_params.get("company", None)
 
-        # 쿼리스트링을 사용한 경우
+        # 쿼리스트링을 사용 : code_request가 조건문 앞에 위치가히 때문에 종목코드를 우선시 하는 로직이다.
         if code_request is not None:  # 조건 검색 로직 필요
-            queryset = CompanyInfo.objects.filter(code=code_request)
+            queryset = CompanyInfo.objects.filter(code=code_request)  # get은 왜 안되는걸까? 리스트형 리턴이라서?
         elif com_name is not None:
             queryset = CompanyInfo.objects.filter(company=com_name)
 
         return queryset
+
 
 
 class DailyPriceViewSet(viewsets.ModelViewSet):
@@ -66,5 +69,26 @@ class DailyPriceViewSet(viewsets.ModelViewSet):
 
 class CompanyStateViewSet(viewsets.ModelViewSet):
     queryset = CompanyState.objects.all()
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = ['code', 'year', 'company_nm']
     serializer_class = CompanyStateSerializer
+
+    # 매번 사용?
+    # def get_queryset(self):
+    #     queryset = CompanyState.objects.all()
+    #     filter_backends = [DjangoFilterBackend]
+    #     filterset_fileds  = ['code', 'year']
+    #     # 종목코드를 검색했을때 빠른 속도로 검색이 되어진 반면 기업명으로 검색했을때는 브라우저가 멈춰버렸다.
+    #     # 서버쪽 페이징처리와 이를 검색할수 있는 알고리즘이 필요하다.
+    #     # request = self.kwargs['year']
+    #     # kwargs = self.kwargs
+    #
+    #     code = self.request.query_params.get("code", None)
+    #     year = self.request.query_params.get("year", None)
+    #
+    #     # & 쿼리를 할 수 있도록 코드 만들기.
+    #     if self.request.query_params:
+    #         queryset = CompanyState.objects.filter(code=code, year=year)
+    #
+    #     return queryset
 
